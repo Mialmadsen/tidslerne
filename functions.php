3722c95080d24dd2a7876d4466f7083dcc7aa9fb
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-05-18 00:18:58 +0300

    NavBar

--- functions.php
+++ functions.php
@@ -25,9 +25,32 @@ function tidslerne_enqueue_assets() {
     );
 
     // Main CSS
-    wp_enqueue_style('main-css', get_stylesheet_uri().'/style.css', array(), '1.0.0');
+    wp_enqueue_style('main-css', get_stylesheet_uri(), array(), '1.0.0');
 }
+add_action('wp_enqueue_scripts', 'tidslerne_enqueue_assets');
+
 function tidslerne_disable_gutenberg_on_pages() {
     remove_post_type_support("page", "editor");
 }
 add_action("init", "tidslerne_disable_gutenberg_on_pages");
+
+function enqueue_search_toggle_script() {
+    wp_enqueue_script(
+        'search-toggle',
+        get_template_directory_uri(). '/JS/search-toggle.js',
+        array(), // dependencies
+        null,    // version
+        true     // load in footer
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_search_toggle_script');
+
+function render_cta_from_post($post_id) {
+    $text = get_field('cta_text', $post_id);
+    $url = get_field('cta_link', $post_id);
+    $style = get_field('cta_style', $post_id);
+
+    if ($text && $url) {
+        include get_template_directory() . '/template-parts/components/button.php';
+    }
+}
