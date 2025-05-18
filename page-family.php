0f6eb156a3f8cd7b57909ca6e38aa87a12eaef02
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-05-18 20:33:18 +0300

    Hero image component

--- page-family.php
+++ page-family.php
@@ -1,19 +1,26 @@
 <?php get_header()?>
- <?php if(have_posts()): ?>
-  <?php while(have_posts()): the_post() ?>
+<?php if(have_posts()): ?>
+<?php while(have_posts()): the_post() ?>
 
-  
-    
-   <?php get_template_part("template-parts/index", "personaStories") ?>
-   <?php get_template_part("template-parts/index", "events") ?>
-   <?php get_template_part("template-parts/index", "newsletter") ?>
-    <?php get_template_part("template-parts/index", "supportLine") ?>   
-   
+<!-- Hero image from component -->
+<?php
+    $background_image = get_field('family_hero'); // ACF Image field (return URL)
+    $heading = get_field('family_heading');
+    include get_template_directory() . '/template-parts/components/hero.php';
+    ?>
 
-   
 
-  <?php endwhile ?>
- <?php endif ?>
+
+<?php get_template_part("template-parts/index", "personaStories") ?>
+<?php get_template_part("template-parts/index", "events") ?>
+<?php get_template_part("template-parts/index", "newsletter") ?>
+<?php get_template_part("template-parts/index", "supportLine") ?>
+
+
+
+
+<?php endwhile ?>
+<?php endif ?>
 
 
 
--- page-personalStories.php
+++ page-personalStories.php
@@ -1,15 +1,24 @@
 <?php get_header()?>
- <?php if(have_posts()): ?>
-  <?php while(have_posts()): the_post() ?>
+<?php if(have_posts()): ?>
+<?php while(have_posts()): the_post() ?>
 
-    <?php get_template_part("template-parts/index", "newsletter") ?>
-    <?php get_template_part("template-parts/read", "moreTreatments") ?>
-   
+<!-- Hero image from component -->
+<?php
+    $background_image = get_field('personal_story_hero'); // ACF Image field (return URL)
+    $heading = get_field('personal_story_heading');
+    include get_template_directory() . '/template-parts/components/hero.php';
+    ?>
 
-   
 
-  <?php endwhile ?>
- <?php endif ?>
+
+<?php get_template_part("template-parts/index", "newsletter") ?>
+<?php get_template_part("template-parts/read", "moreTreatments") ?>
+
+
+
+
+<?php endwhile ?>
+<?php endif ?>
 
 
 
