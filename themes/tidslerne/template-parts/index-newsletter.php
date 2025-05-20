782e0e2f63cddf5f1f3bf69a9e8018f95e10a43c
Mialmadsen <miamad01@easv365.dk>
2025-05-20 10:31:57 +0200

    Newsletter design done

---
+++ themes/tidslerne/template-parts/index-newsletter.php
@@ -0,0 +1,15 @@
+<section class="newsletter">
+  <div class="newsletter-content">
+    <div class="letter-icon">
+      <!-- SVG-ikon for kuvert -->
+      <i class="fa-regular fa-envelope" style="color: #55a646;"></i>
+    </div>
+    <div class="text">
+      <h2>Nyhedsbrev</h2>
+      <p>Tilmeld dig vores nyhedsbrev og få besked om nye artikler og events, direkte i din indbakke</p>
+    </div>
+    <div class="button-container">
+      <button>Tilmeld dig</button>
+    </div>
+  </div>
+</section>
